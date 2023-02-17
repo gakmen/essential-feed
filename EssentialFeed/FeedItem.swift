3b74dfc891b5b7e6d34d14f3cb3a4fe631f68bcd
Gosha Akmen <gakmen@icloud.com>
2023-02-17 16:33:23 +0500

    создал модель FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by  Gosha Akmen on 17.02.2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imgeURL: URL
+    
+}

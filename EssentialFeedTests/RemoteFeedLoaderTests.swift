ab6120aaa2388aee4e53be5b8a238b51958fb046
Gosha Akmen <gakmen@icloud.com>
2023-02-18 11:56:42 +0500

    запрос данных по УРЛу в методе `load`

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,14 +9,32 @@ import Foundation
 import XCTest
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
+    
     var requestedURL: URL?
+}
+
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://a-url.com")
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     
-    func test_init() {
-        let client = HTTPClient()
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient.shared
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }

eb50b6acc2acbe1dde049f50226daf1160864dc9
Gosha Akmen <gakmen@icloud.com>
2023-02-24 13:35:09 +0500

    поставляет массив FeedItem при 200ом ответе с заполненным JSONом

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -8,9 +8,24 @@
 import Foundation
 
 public struct FeedItem: Equatable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let imgeURL: URL
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
     
+    public init(id: UUID, description: String?, location: String?, imgeURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imgeURL
+    }
+}
+
+extension FeedItem: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case location
+        case imageURL = "image"
+    }
 }

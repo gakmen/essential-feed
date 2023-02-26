9b398c902aacf8f4a962b0cb77b77892dc254861
Gosha Akmen <gakmen@icloud.com>
2023-02-26 12:44:37 +0500

    переместил файл с тестами, чтобы показать к какому модулю он принадлежит (Feed API)

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -29,7 +29,7 @@ public final class RemoteFeedLoader {
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { [weak self] result in
             guard self != nil else { return }
-            
+             
             switch result {
             case let .success(data, response):
                 completion(FeedItemsMapper.map(data, from: response))

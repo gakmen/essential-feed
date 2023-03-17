dec46a61dd63bfa319eb9668346fe010cc469779
Gosha Akmen <gakmen@icloud.com>
2023-03-17 23:21:05 +0500

    повысил время ожидания отклика от сервера в тесте

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -42,7 +42,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
             receivedResult = result
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 5.0)
+        wait(for: [exp], timeout: 8.0)
         
         return receivedResult
     }

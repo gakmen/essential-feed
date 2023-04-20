800c5c1101d70ff2136dd3cadca55cd938d8210b
Gosha Akmen <gakmen@icloud.com>
2023-04-20 22:47:46 +0500

    LocalFeedLoader во время инициализации не отправляет сообщения store'у (прежде чем валидировать кэш)

---
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,33 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by  Gosha Akmen on 20.04.2023.
+//
+
+import XCTest
+import EssentialFeed
+
+final class ValidateFeedCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotStoreMessageUponCreation() {
+        let (store, _) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+
+    //MARK: Helpers
+    
+    private func makeSUT (
+        currentDate: @escaping () -> Date = Date.init,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (store: FeedStoreSpy, sut: LocalFeedLoader) {
+        
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (store, sut)
+    }
+}

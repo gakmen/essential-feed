e0c3f4698d176a4e5512c82d733327990c75a40d
Gosha Akmen <gakmen@icloud.com>
2023-04-21 23:20:58 +0500

    Разбил функциональные сегменты на расширения

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -19,6 +19,16 @@ public final class LocalFeedLoader {
         self.currentDate = currentDate
     }
     
+    private let maxCacheAgeInDays: Int = 7
+    
+    private func validate(_ timestamp: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
+        return currentDate() < maxCacheAge
+    }
+}
+
+ 
+extension LocalFeedLoader {
     public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed { [weak self] error in
             guard let self else { return }
@@ -36,7 +46,9 @@ public final class LocalFeedLoader {
             completion(error)
         }
     }
-    
+}
+
+extension LocalFeedLoader {
     public func load(completion: @escaping (LoadResult) -> Void) {
         store.retrieve { [weak self] result in
             guard let self else { return }
@@ -52,7 +64,9 @@ public final class LocalFeedLoader {
             }
         }
     }
+}
 
+extension LocalFeedLoader {
     public func validateCache() {
         store.retrieve { [weak self] result in
             guard let self else { return }
@@ -65,13 +79,6 @@ public final class LocalFeedLoader {
                 break
             }
         }
-    }
-    
-    private let maxCacheAgeInDays: Int = 7
-    
-    private func validate(_ timestamp: Date) -> Bool {
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
-        return currentDate() < maxCacheAge
     }
 }
 

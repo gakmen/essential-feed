1adf6d365b7186a6da75bdc3f35376fb4b1fed45
Gosha Akmen <gakmen@icloud.com>
2023-04-26 10:12:56 +0500

    Выгрузка после загрузки в пустой кэш возвращает загруженные значения.

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,14 +8,62 @@
 import XCTest
 import EssentialFeed
 
+@available(macOS 13.0, *)
 class CodableFeedStore {
+    private struct Cache: Codable {
+        let feed: [LocalFeedImage]
+        let timestamp: Date
+    }
+    
+    private let storeURL: URL = FileManager
+        .default
+        .urls(for: .documentDirectory, in: .userDomainMask)
+        .first!
+        .appending(path: "image-feed.store")
     
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.empty)
+        guard let data = try? Data(contentsOf: storeURL) else {
+            return completion(.empty)
+        }
+        let decoder = JSONDecoder()
+        let cache = try! decoder.decode(Cache.self, from: data)
+        completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+    }
+    
+    func insert(_ feed: [LocalFeedImage], _ timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        let encoder = JSONEncoder()
+        let encoded = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+        try! encoded.write(to: storeURL)
+        completion(nil)
     }
 }
 
+@available(macOS 13.0, *)
 final class CodableFeedStoreTests: XCTestCase {
+    
+    override class func setUp() {
+        super.setUp()
+        
+        let storeURL: URL = FileManager
+            .default
+            .urls(for: .documentDirectory, in: .userDomainMask)
+            .first!
+            .appending(path: "image-feed.store")
+        
+        try? FileManager.default.removeItem(at: storeURL)
+    }
+    
+    override class func tearDown() {
+        super.tearDown()
+        
+        let storeURL: URL = FileManager
+            .default
+            .urls(for: .documentDirectory, in: .userDomainMask)
+            .first!
+            .appending(path: "image-feed.store")
+        
+        try? FileManager.default.removeItem(at: storeURL)
+    }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = CodableFeedStore()
@@ -45,6 +93,30 @@ final class CodableFeedStoreTests: XCTestCase {
                     break
                 default:
                     XCTFail("Ожидали, что при попытке дважды выгрузить пустой кэш оба раза получим пустой результат, вместо этого получили \(firstResult) и \(secondResult)")
+                }
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_retrieveAfterInseringCache_deliversInsertedValues() {
+        let sut = CodableFeedStore()
+        let feed = uniqueImageFeed()
+        let timestamp = Date()
+        let exp = expectation(description: "Ждём выгрузки кэша")
+        
+        sut.insert(feed.local, timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Ожидали успешную вставку картинок")
+            
+            sut.retrieve() { retrieveResult in
+                switch retrieveResult {
+                case let .found(retrievedFeed, retrievedTimestamp):
+                    XCTAssertEqual(retrievedFeed, feed.local)
+                    XCTAssertEqual(retrievedTimestamp, timestamp)
+                default:
+                    XCTFail("Ожидали результат .found с картинками \(feed) и временем \(timestamp), вместо этого получили \(retrieveResult)")
                 }
                 exp.fulfill()
             }

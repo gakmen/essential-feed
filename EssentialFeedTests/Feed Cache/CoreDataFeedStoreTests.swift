410702735791245abac0287c56f63ea1bc8bbc3d
Gosha Akmen <gakmen@icloud.com>
2023-05-19 14:43:39 +0500

    Удалил плейсхолдеры функций

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -10,50 +10,50 @@ import XCTest
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        <#code#>
+        
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        <#code#>
+        
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-        <#code#>
+        
     }
     
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-        <#code#>
+        
     }
     
     func test_insert_deliversNoErrorOnEmptyCache() {
-        <#code#>
+        
     }
     
     func test_insert_deliversNoErrorOnNonEmptyCache() {
-        <#code#>
+        
     }
     
     func test_insert_overridesPreviouslyInsertedCacheValues() {
-        <#code#>
+        
     }
     
     func test_delete_deliversNoErrorOnEmptyCache() {
-        <#code#>
+        
     }
     
     func test_delete_hasNoSideEffectsOnEmptyCache() {
-        <#code#>
+        
     }
     
     func test_delete_deliversNoErrorOnNonEmptyCache() {
-        <#code#>
+        
     }
     
     func test_delete_emptiesPreviouslyInsertedCache() {
-        <#code#>
+        
     }
     
     func test_storeSideEffects_runSerially() {
-        <#code#>
+        
     }
 }

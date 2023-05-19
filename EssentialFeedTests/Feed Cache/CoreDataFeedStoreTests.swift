9b84c94883d4244e0c413529ee2e0bc1018d2d97
Gosha Akmen <gakmen@icloud.com>
2023-05-19 14:42:36 +0500

    - сгруппировал feed store specs и вспомогательные функции - сделал файл с пустыми методами CoreDataFeedStoreSpecs

---
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,59 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by  Gosha Akmen on 17.05.2023.
+//
+
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        <#code#>
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        <#code#>
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        <#code#>
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        <#code#>
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        <#code#>
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        <#code#>
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        <#code#>
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        <#code#>
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        <#code#>
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        <#code#>
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        <#code#>
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        <#code#>
+    }
+}

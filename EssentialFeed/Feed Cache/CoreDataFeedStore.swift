c06725357c3cf2f042f3aaa3ed364beecaf684a7
Gosha Akmen <gakmen@icloud.com>
2023-05-22 09:53:46 +0500

    'CoreDateFeedStore.retrieve()' возвращает .empty при пустом кэше

---
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,25 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by  Gosha Akmen on 22.05.2023.
+//
+
+import Foundation
+
+public final class CoreDataFeedStore: FeedStore {
+    
+    public init() {}
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], _ timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+}

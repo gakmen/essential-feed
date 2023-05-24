abf420de8d6f018913eb81b6a981ce91322cd4f7
Gosha Akmen <gakmen@icloud.com>
2023-05-24 10:36:54 +0500

    'CoreDataFeedStore.retrieve()' возвращает найденные значения при не пустом кэше

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -24,7 +24,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatRetriveDeliversFoundValuesOnNonEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {

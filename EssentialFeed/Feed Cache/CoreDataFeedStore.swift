9a0e4eac6dab28eee8a1ac4f3cba94953c053329
Gosha Akmen <gakmen@icloud.com>
2023-05-25 10:20:14 +0500

    Перенес логику запроса на получение кэша во вспомогательный метод внутри 'ManagedCache'

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -21,9 +21,7 @@ public final class CoreDataFeedStore: FeedStore {
         let context = self.context
         context.perform {
             do {
-                let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
-                request.returnsObjectsAsFaults = false
-                if let cache = try context.fetch(request).first {
+                if let cache = try ManagedCache.find(in: context) {
                     completion (.found(feed: cache.localFeed, timestamp: cache.timestamp))
                 } else {
                     completion(.empty)
@@ -60,6 +58,13 @@ public final class CoreDataFeedStore: FeedStore {
         
         var localFeed: [LocalFeedImage] {
             return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+        }
+        
+        static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+            let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+            request.returnsObjectsAsFaults = false
+            let cache = try context.fetch(request).first
+            return cache
         }
     }
     

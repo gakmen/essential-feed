d5f44f795d03a07bdf5974585573fbe71a2923b4
Gosha Akmen <gakmen@icloud.com>
2023-05-28 23:01:01 +0500

    Разделил при помощи расширений данные классов моделей и вспомогательные методы.

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -9,10 +9,12 @@ import CoreData
 
 @objc(ManagedCache)
 internal class ManagedCache: NSManagedObject {
-    @NSManaged var timestamp: Date
-    @NSManaged var feed: NSOrderedSet
-    
-    var localFeed: [LocalFeedImage] {
+    @NSManaged internal var timestamp: Date
+    @NSManaged internal var feed: NSOrderedSet
+}
+
+extension ManagedCache {
+    internal var localFeed: [LocalFeedImage] {
         return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
     }
     
@@ -23,7 +25,7 @@ internal class ManagedCache: NSManagedObject {
         return cache
     }
     
-    static func createNewUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+    internal static func createNewUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
         try find(in: context).map(context.delete)
         return ManagedCache(context: context)
     }
--- EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -9,17 +9,19 @@ import CoreData
 
 @objc(ManagedFeedImage)
 internal class ManagedFeedImage: NSManagedObject {
-    @NSManaged var id: UUID
-    @NSManaged var imageDescription: String?
-    @NSManaged var location: String?
-    @NSManaged var url: URL
-    @NSManaged var cache: ManagedCache
-    
-    var local: LocalFeedImage {
+    @NSManaged internal var id: UUID
+    @NSManaged internal var imageDescription: String?
+    @NSManaged internal var location: String?
+    @NSManaged internal var url: URL
+    @NSManaged internal var cache: ManagedCache
+}
+
+extension ManagedFeedImage {
+    internal var local: LocalFeedImage {
         return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
     }
     
-    static func getImages(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+    internal static func getImages(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
         return NSOrderedSet (
             array: localFeed.map { local in
                 let managed = ManagedFeedImage(context: context)

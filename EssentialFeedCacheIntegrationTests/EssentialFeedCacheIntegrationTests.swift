6504897b720d5b2a6ada2ea444986a50a3aa3292
Gosha Akmen <gakmen@icloud.com>
2023-05-31 10:28:59 +0500

    Create 'EssentialFeedCacheIntegrationTests' target to separate potentially slow integration tests from the fast isolated unit tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by  Gosha Akmen on 31.05.2023.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+}

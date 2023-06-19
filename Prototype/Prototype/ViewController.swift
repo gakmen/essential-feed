c8a9d3ec15fd1eb6c3cbd613c36416954454f41a
Gosha Akmen <gakmen@icloud.com>
2023-06-19 22:46:02 +0300

    Add empty project for prototype

---
+++ Prototype/Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by  Gosha Akmen on 19.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

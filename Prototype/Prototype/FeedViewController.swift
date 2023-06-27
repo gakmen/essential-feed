1c10c1a60e683cee61682370add9fdcbcd602848
Gosha Akmen <gakmen@icloud.com>
2023-06-27 09:17:46 +0500

    Added `numberOfRowsInSection` method to render the table view

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 final class FeedViewController: UITableViewController {
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 10
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {

a634e445d0e0153a42b8efc670efe1d1221773ac
Gosha Akmen <gakmen@icloud.com>
2023-07-20 16:32:06 +0500

    Instantiate `FeedViewController` from Feed storyboard.

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -9,15 +9,10 @@ import UIKit
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching  {
     
-    private var refreshController: FeedRefreshViewController?
-    private var imageLoader: FeedImageDataLoader?
+    var refreshController: FeedRefreshViewController?
+    
     var tableModel = [FeedImageCellController]() {
         didSet { tableView.reloadData() }
-    }
-    
-    convenience init (refreshController: FeedRefreshViewController) {
-        self.init()
-        self.refreshController = refreshController
     }
     
     public override func viewDidLoad() {

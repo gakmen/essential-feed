236d3228dcaf34770d1a1850970df2cea27177cd
Gosha Akmen <gakmen@icloud.com>
2023-07-27 17:42:36 +0500

    Move all composition helpers to separate files.

---
+++ EssentialFeediOS/Feed UI/Composers/FeedImageDataLoaderPresentationAdapter.swift
@@ -0,0 +1,42 @@
+//
+//  FeedImageDataLoaderPresentationAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by  Gosha Akmen on 27.07.2023.
+//
+
+import EssentialFeed
+
+final class FeedImageDataLoaderPresentationAdapter <View: FeedImageView, Image>:
+    FeedImageCellControllerDelegate where View.Image == Image {
+    
+    private let model: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    
+    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    private var task: FeedImageDataLoaderTask?
+    
+    var presenter: FeedImagePresenter<View, Image>?
+    
+    func didRequestImage() {
+        presenter?.didStartLoadingImageData(for: model)
+        
+        task = imageLoader.loadImageData(from: model.url) { [weak self] result in
+            guard let self else {return}
+            switch result {
+            case let .success(data):
+                self.presenter?.didFinishLoadingImageData(with: data, for: self.model)
+            case let .failure(error):
+                self.presenter?.didFinishLoadingImageData(with: error, for: self.model)
+            }
+        }
+    }
+    
+    func didCancelImageRequest() {
+        task?.cancel()
+    }
+}
---
+++ EssentialFeediOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,30 @@
+//
+//  FeedLoaderPresentationAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by  Gosha Akmen on 27.07.2023.
+//
+
+import EssentialFeed
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    private let feedLoader: FeedLoader
+    var presenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        presenter?.didStartLoadingFeed()
+        
+        feedLoader.load { [weak presenter] result in
+            switch result {
+            case let .success(feed):
+                presenter?.didFinishLoadingFeed(with: feed)
+            case let .failure(error):
+                presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}

72fb6ef252bcaebf7908f4b79da41567092b54e2
Gosha Akmen <gakmen@icloud.com>
2023-08-02 17:15:44 +0500

    Move `FeedErrorViewModel` to a separate file.

---
+++ EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by  Gosha Akmen on 02.08.2023.
+//
+
+//import Foundation
+
+struct FeedErrorViewModel {
+    var message: String?
+}
+

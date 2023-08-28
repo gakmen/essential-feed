dd96f2ce0f4140b265c6136012621ec6b4e9d11d
Gosha Akmen <gakmen@icloud.com>
2023-08-28 16:24:09 +0500

    FeedLoaderWithFallbackComposite.load` delivers error on both primary and fallback failure.

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -32,7 +32,7 @@ class FeedLoaderWithFallbackComposite: FeedLoader {
 
 class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     
-    func test_deliversPrimaryFeedOnPrimarySuccess() {
+    func test_load_deliversPrimaryFeedOnPrimarySuccess() {
         
         let primaryFeed = uniqueFeed()
         let fallbackFeed = uniqueFeed()
@@ -41,12 +41,18 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         expect(sut, toCompleteWith: .success(primaryFeed))
     }
     
-    func test_deliversFallbackFeedOnPrimaryFailure() {
+    func test_load_deliversFallbackFeedOnPrimaryFailure() {
         
         let fallbackFeed = uniqueFeed()
         let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
         
         expect(sut, toCompleteWith: .success(fallbackFeed))
+    }
+    
+    func test_load_deliversErrorOnBothPrimaryAndFallbackLoaderFailure() {
+        let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .failure(anyNSError()))
+        
+        expect(sut, toCompleteWith: .failure(anyNSError()))
     }
     
     //MARK: - Helpers

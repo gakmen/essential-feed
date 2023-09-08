feae29167fd1c5ff3ee211a32f57a3e0913b876c
Gosha Akmen <gakmen@icloud.com>
2023-09-08 16:12:58 +0500

    Move `InMemoryFeedStore` to a shared scope in a separate file

---
+++ EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
@@ -0,0 +1,62 @@
+//
+//  InMemoryFeedStore.swift
+//  EssentialAppTests
+//
+//  Created by Георгий Акмен on 08.09.2023.
+//
+
+import EssentialFeed
+
+class InMemoryFeedStore {
+    
+    private(set) var feedCache: CachedFeed?
+    private var feedImageDataCache: [URL: Data] = [:]
+    
+    init(feedCache: CachedFeed? = nil) {
+        self.feedCache = feedCache
+    }
+}
+
+extension InMemoryFeedStore: FeedStore {
+    
+    func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
+        feedCache = nil
+        completion(.success(()))
+    }
+    
+    func insert(_ feed: [LocalFeedImage], _ timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        feedCache = CachedFeed(feed: feed, timestamp: timestamp)
+        completion(.success(()))
+    }
+    
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.success(feedCache))
+    }
+}
+
+extension InMemoryFeedStore: FeedImageDataStore {
+    
+    func insert(image data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        feedImageDataCache[url] = data
+        completion(.success(()))
+    }
+    
+    func retrieve(dataFor url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(feedImageDataCache[url]))
+    }
+}
+
+extension FeedImageDataStore {
+    
+    static var empty: InMemoryFeedStore {
+        InMemoryFeedStore()
+    }
+    
+    static var withExpiredFeedCache: InMemoryFeedStore {
+        InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date.distantPast))
+    }
+    
+    static var withNonExpiredFeedCache: InMemoryFeedStore {
+        InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date()))
+    }
+}

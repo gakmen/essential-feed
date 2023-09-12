6aba4afe242b4a8e5e5a1cbf1a00049088697b2c
Gosha Akmen <gakmen@icloud.com>
2023-09-12 11:22:38 +0500

    Move `XCTestCase` snapshot helpers to a separate file

--- EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -92,64 +92,6 @@ class FeedSnapshotTests: XCTestCase {
             )
         ]
     }
-    
-    private func assert(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
-        let snapshotData = makeSnapshotData(from: snapshot)
-        let snapshotURL = makeSnapshotURL(named: name)
-        
-        guard let storedSnapshotData = try? Data(contentsOf: snapshotURL) else {
-            XCTFail(
-                "Failed to load snapshot from url: \(snapshotURL). Use `record` method to store a snapshot before asserting",
-                file: file,
-                line: line
-            )
-            return
-        }
-        
-        if snapshotData != storedSnapshotData {
-            let temporarySnapshotURL = URL(filePath: NSTemporaryDirectory())
-                .appending(component: snapshotURL.lastPathComponent)
-            try? snapshotData?.write(to: temporarySnapshotURL)
-            
-            XCTFail(
-                "New snapshot does not match stored snapshot. New snapshot URL: \(temporarySnapshotURL). Stored snapshot URL: \(snapshotURL)",
-                file: file,
-                line: line
-            )
-        }
-        
-    }
-    
-    private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
-        let snapshotData = makeSnapshotData(from: snapshot)
-        let snapshotURL = makeSnapshotURL(named: name)
-        
-        do {
-            try FileManager.default.createDirectory(
-                at: snapshotURL.deletingLastPathComponent(),
-                withIntermediateDirectories: true
-            ) 
-            try snapshotData?.write(to: snapshotURL)
-        } catch {
-            XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
-        }
-    }
-    
-    private func makeSnapshotURL(named name: String, file: StaticString = #file) -> URL {
-        let snapshotURL = URL(filePath: String(describing: file))
-            .deletingLastPathComponent()
-            .appending(component: "snapshots")
-            .appending(component: "\(name).png")
-        return snapshotURL
-    }
-    
-    private func makeSnapshotData(from snapshot: UIImage,  file: StaticString = #file, line: UInt = #line) -> Data? {
-        guard let snapshotData = snapshot.pngData() else {
-            XCTFail("Failed to generate PNG data from a snapshot", file: file, line: line)
-            return nil
-        }
-        return snapshotData
-    }
 }
 
 private extension FeedViewController {

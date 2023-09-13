f68efe837f56102ef4e8c602fbd08b5f373af355
Gosha Akmen <gakmen@icloud.com>
2023-09-13 21:49:05 +0500

    Execute RunLoop to prevent memory leaks in the test

--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Assertions.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Assertions.swift
@@ -5,71 +5,78 @@
 //  Created by  Gosha Akmen on 13.09.2023.
 //
 
-import UIKit
 import XCTest
 import EssentialFeed
 import EssentialFeediOS
 
-func assertThat (
-    _ sut: FeedViewController,
-    isRendering feed: [FeedImage],
-    file: StaticString = #file,
-    line: UInt = #line
-){
-    sut.view.enforceLayoutCycle()
-    
-    XCTAssertEqual (
-        sut.numberOfRenderedFeedImageViews(),
-        feed.count,
-        "Expected number of images to be \(sut.numberOfRenderedFeedImageViews()), got \(feed.count) instead",
-        file: file,
-        line: line
-    )
-    
-    feed.enumerated().forEach { index, image in
-        assertThat(sut, hasViewConfiguredFor: image, at: index, file: file, line: line)
+extension FeedUIIntegrationTests {
+    func assertThat (
+        _ sut: FeedViewController,
+        isRendering feed: [FeedImage],
+        file: StaticString = #file,
+        line: UInt = #line
+    ){
+        sut.view.enforceLayoutCycle()
+        
+        XCTAssertEqual (
+            sut.numberOfRenderedFeedImageViews(),
+            feed.count,
+            "Expected number of images to be \(sut.numberOfRenderedFeedImageViews()), got \(feed.count) instead",
+            file: file,
+            line: line
+        )
+        
+        feed.enumerated().forEach { index, image in
+            assertThat(sut, hasViewConfiguredFor: image, at: index, file: file, line: line)
+        }
+        
+        executeRunLoopToCleanUpReferences()
     }
-}
 
-func assertThat (
-    _ sut: FeedViewController,
-    hasViewConfiguredFor image: FeedImage,
-    at index: Int,
-    file: StaticString = #file,
-    line: UInt = #line
-){
-    let view = sut.feedImageView(at: index)
-    
-    guard let cell = view as? FeedImageCell else {
-        return XCTFail (
-            "Expected \(FeedImageCell.self) instance, got \(String(describing: view)) instead",
+    func assertThat (
+        _ sut: FeedViewController,
+        hasViewConfiguredFor image: FeedImage,
+        at index: Int,
+        file: StaticString = #file,
+        line: UInt = #line
+    ){
+        let view = sut.feedImageView(at: index)
+        
+        guard let cell = view as? FeedImageCell else {
+            return XCTFail (
+                "Expected \(FeedImageCell.self) instance, got \(String(describing: view)) instead",
+                file: file,
+                line: line
+            )
+        }
+        
+        let shouldLocationBeVisible = (image.location != nil)
+        XCTAssertEqual (
+            cell.isShowingLocation,
+            shouldLocationBeVisible,
+            "Expected `isShowingLocation` to be \(shouldLocationBeVisible) for image view at index \(index)",
+            file: file,
+            line: line
+        )
+        
+        XCTAssertEqual (
+            cell.locationText,
+            image.location,
+            "Expected location text to be \(String(describing: image.location)) for image view at index \(index)",
+            file: file,
+            line: line
+        )
+        
+        XCTAssertEqual (
+            cell.descriptionText,
+            image.description,
+            "Expected description text to be \(String(describing: image.description)) for image view at index \(index)",
             file: file,
             line: line
         )
     }
     
-    let shouldLocationBeVisible = (image.location != nil)
-    XCTAssertEqual (
-        cell.isShowingLocation,
-        shouldLocationBeVisible,
-        "Expected `isShowingLocation` to be \(shouldLocationBeVisible) for image view at index \(index)",
-        file: file,
-        line: line
-    )
-    
-    XCTAssertEqual (
-        cell.locationText,
-        image.location,
-        "Expected location text to be \(String(describing: image.location)) for image view at index \(index)",
-        file: file,
-        line: line
-    )
-    
-    XCTAssertEqual (
-        cell.descriptionText,
-        image.description,
-        "Expected description text to be \(String(describing: image.description)) for image view at index \(index)",
-        file: file,
-        line: line
-    )
+    private func executeRunLoopToCleanUpReferences() {
+        RunLoop.current.run(until: Date())
+    }
 }

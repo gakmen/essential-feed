294ba283e2856e122467ee5922f01d582f1995f8
Gosha Akmen <gakmen@icloud.com>
2025-01-06 16:12:20 +0500

    Move `context.perform` call up

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -11,78 +11,88 @@ import EssentialFeed
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 
   func test_retrieve_deliversEmptyOnEmptyCache() {
-    let sut = makeSUT()
-
-    assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+    makeSUT { sut in
+      self.assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+    }
   }
 
   func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-    let sut = makeSUT()
-
-    assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
+    makeSUT { sut in
+      self.assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
+    }
   }
 
   func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-    let sut = makeSUT()
-
-    assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
+    makeSUT { sut in
+      self.assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
+    }
   }
 
   func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-    let sut = makeSUT()
-
-    assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
+    makeSUT { sut in
+      self.assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
+    }
   }
 
   func test_insert_deliversNoErrorOnEmptyCache() {
-    let sut = makeSUT()
-
-    assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+    makeSUT { sut in
+      self.assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+    }
   }
 
   func test_insert_deliversNoErrorOnNonEmptyCache() {
-    let sut = makeSUT()
-
-    assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+    makeSUT { sut in
+      self.assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
   }
 
   func test_insert_overridesPreviouslyInsertedCacheValues() {
-    let sut = makeSUT()
-
-    assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
+    makeSUT { sut in
+      self.assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
+    }
   }
 
   func test_delete_deliversNoErrorOnEmptyCache() {
-    let sut = makeSUT()
-
-    assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+    makeSUT { sut in
+      self.assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+    }
   }
 
   func test_delete_hasNoSideEffectsOnEmptyCache() {
-    let sut = makeSUT()
-
-    assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
+    makeSUT { sut in
+      self.assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
+    }
   }
 
   func test_delete_deliversNoErrorOnNonEmptyCache() {
-    let sut = makeSUT()
-
-    assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
+    makeSUT { sut in
+      self.assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
   }
 
   func test_delete_emptiesPreviouslyInsertedCache() {
-    let sut = makeSUT()
-
-    assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
+    makeSUT { sut in
+      self.assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
+    }
   }
 
   // - MARK: Helpers
 
-  private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
+  private func makeSUT(
+    _ test: @escaping (CoreDataFeedStore) -> Void,
+    file: StaticString = #filePath,
+    line: UInt = #line
+  ) {
     let storeURL = URL(fileURLWithPath: "/dev/null")
     let sut = try! CoreDataFeedStore(storeURL: storeURL)
     trackForMemoryLeaks(sut, file: file, line: line)
-    return sut
+
+    let exp = expectation(description: "retrieve")
+    sut.perform {
+      test(sut)
+      exp.fulfill()
+    }
+    wait(for: [exp], timeout: 0.1)
   }
 
 }
